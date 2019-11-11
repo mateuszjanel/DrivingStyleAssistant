9cfe235711e82d7da5bea43d15f1d849e1f6a193
mateuszjanel <janel.mateusz@gmail.com>
2019-11-11 14:49:26 +0100

    Keeping last values to further analysis

---
+++ app/src/main/java/com/example/drivingstyleassistant/common/LastValuesStorage.java
@@ -0,0 +1,12 @@
+package com.example.drivingstyleassistant.common;
+
+import java.util.Date;
+
+public class LastValuesStorage {
+    Date timestamp;
+    float lastSpeedValue;
+    float lastAccelerationValueX;
+    float lastAccelerationValueY;
+    float lastAccelerationValueZ;
+    
+}
