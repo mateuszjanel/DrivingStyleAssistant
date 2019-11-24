75a2067f9acae44245f6a2eba8259c4b5492c7f5
mateuszjanel <janel.mateusz@gmail.com>
2019-11-24 16:13:27 +0100

    Softness grade, LOESS code

--- app/src/main/java/com/example/drivingstyleassistant/data/SoftnessGrade.java
+++ app/src/main/java/com/example/drivingstyleassistant/data/SoftnessGrade.java
@@ -1,12 +1,32 @@
 package com.example.drivingstyleassistant.data;
 
+import org.apache.commons.math3.analysis.interpolation.LoessInterpolator;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SoftnessGrade implements GradeHelper {
 
-    float grade(){
+    public ArrayList<Double> speeds;
+    public ArrayList<Double> xValsList;
+    public double[] smooths;
+
+    public float grade(){
 
     }
 
-    void analyze(){
+    public void analyze(){
+        LoessInterpolator loessInterpolator = new LoessInterpolator();
+        smooths = loessInterpolator.smooth(arrayListToDoubleArray(xValsList), arrayListToDoubleArray(speeds));
 
+    }
+
+    public double[] arrayListToDoubleArray(ArrayList<Double> arrayList){
+        int arraySize = arrayList.size();
+        double[] tempDoubleArray = new double[arraySize];
+        for(int i = 0; i<arraySize; i++){
+            tempDoubleArray[i] = (double)arrayList.get(i);
+        }
+        return tempDoubleArray;
     }
 }
